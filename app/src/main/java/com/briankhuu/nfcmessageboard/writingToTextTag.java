79b4aef53d7608856f28f12cfd5b359b81db01b0
mofosyne <mofosyne@gmail.com>
2016-09-26 01:58:24 +1000

    updated support package to com.android.support:appcompat-v7:22.1.1 and also added a new activity to start seperating actions

---
+++ app/src/main/java/com/briankhuu/nfcmessageboard/writingToTextTag.java
@@ -0,0 +1,13 @@
+package com.briankhuu.nfcmessageboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class writingToTextTag extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_writing_to_text_tag);
+    }
+}
